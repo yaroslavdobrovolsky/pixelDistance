3b3f37c97308367f5ce00887f45df45913101025
yaroslav <arduino.yan@gmail.com>
2023-06-15 23:51:14 +0700

    few changes

--- consts.py
+++ consts.py
@@ -4,3 +4,4 @@ LINE_COLOR = (255, 50, 50)
 FONT_COLOR = (0, 255, 0)
 BACKGROUND_FONT_COLOR = (50, 50, 255)
 FONT_SIZE = 15
+FONT = "freesansbold.ttf"
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ class App:
 
         pygame.init()
 
-        self.font = pygame.font.Font("freesansbold.ttf", consts.FONT_SIZE)
+        self.font = pygame.font.Font(consts.FONT, consts.FONT_SIZE)
 
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         pygame.display.set_caption(consts.CAPTION)
 
         self.distance = 0
         self.cursor_pos = pygame.mouse.get_pos()
-        self.point_position = self.cursor_pos
+        self.point_positions = [self.cursor_pos, self.cursor_pos]
 
         while True:
             for event in pygame.event.get():
@@ -45,35 +45,37 @@ class App:
 
     def make_point(self):
         if pygame.mouse.get_pressed()[0]:
-            self.point_position = self.cursor_pos
+            self.point_positions[0] = self.cursor_pos
+        if pygame.mouse.get_pressed()[2]:
+            self.point_positions[1] = self.cursor_pos
 
     def get_distance(self):
-        self.distance = int(math.sqrt(abs(self.point_position[0] - self.cursor_pos[0]) ** 2 +
-                                      abs(self.point_position[1] - self.cursor_pos[1]) ** 2)) + 1
+        self.distance = int(math.sqrt(abs(self.point_positions[0][0] - self.point_positions[1][0]) ** 2 +
+                                      abs(self.point_positions[0][1] - self.point_positions[1][0]) ** 2)) + 1
 
     def draw_line_between_point_and_cursor(self):
-        pygame.draw.line(self.screen, consts.LINE_COLOR, self.point_position, self.cursor_pos, 1)
+        pygame.draw.line(self.screen, consts.LINE_COLOR, self.point_positions[0], self.point_positions[1], 1)
 
     def print_distance_on_line(self):
         text = self.font.render(str(self.distance), True, consts.FONT_COLOR, consts.BACKGROUND_FONT_COLOR)
         text_rect = text.get_rect()
-        text_rect.center = ((self.point_position[0] + self.cursor_pos[0]) / 2,
-                            (self.point_position[1] + self.cursor_pos[1]) / 2)
+        text_rect.center = ((self.point_positions[0][0] + self.point_positions[1][0]) / 2,
+                            (self.point_positions[0][1] + self.point_positions[1][1]) / 2)
 
         self.screen.blit(text, text_rect)
 
     def print_coordinates_on_points(self):
         # todo: make coordinate render with multiple lines
 
-        first_point_coordinates_text = self.font.render(str(self.point_position), True,
+        first_point_coordinates_text = self.font.render(str(self.point_positions[0]), True,
                                                         consts.FONT_COLOR, consts.BACKGROUND_FONT_COLOR)
         first_point_coordinates_text_rect = first_point_coordinates_text.get_rect()
-        first_point_coordinates_text_rect.center = self.point_position
+        first_point_coordinates_text_rect.center = self.point_positions[0]
 
-        second_point_coordinates_text = self.font.render(str(self.cursor_pos), True,
+        second_point_coordinates_text = self.font.render(str(self.point_positions[1]), True,
                                                          consts.FONT_COLOR, consts.BACKGROUND_FONT_COLOR)
         second_point_coordinates_text_rect = second_point_coordinates_text.get_rect()
-        second_point_coordinates_text_rect.center = self.cursor_pos
+        second_point_coordinates_text_rect.center = self.point_positions[1]
 
         self.screen.blit(first_point_coordinates_text, first_point_coordinates_text_rect)
         self.screen.blit(second_point_coordinates_text, second_point_coordinates_text_rect)
